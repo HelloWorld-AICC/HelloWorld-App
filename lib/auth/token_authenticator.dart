3062d57903ea488309e0dc27592c7961dd43b19d
Yunseo Lee <lys139011@gmail.com>
2024-08-28 09:18:55 +0900

    feat: Token Management

---
+++ lib/auth/token_authenticator.dart
@@ -0,0 +1,77 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../route/route_service.dart';
+
+class TokenAuthenticator {
+  final Dio _dio;
+  final String _baseUrl;
+  final String _tokenRefreshUrl;
+  final RouteService _routeService;
+
+  TokenAuthenticator(
+      this._dio, this._baseUrl, this._tokenRefreshUrl, this._routeService);
+
+  Future<RequestOptions> authenticate(
+      RequestOptions requestOptions, Response response) async {
+    final isPathRefresh =
+        requestOptions.uri.toString() == '$_baseUrl$_tokenRefreshUrl';
+
+    if (response.statusCode == 401 && !isPathRefresh) {
+      final tokenRefreshSuccess = await _fetchUpdateToken();
+
+      if (tokenRefreshSuccess) {
+        final newToken = await _fetchAccessToken();
+        requestOptions.headers['Authorization'] = 'Bearer $newToken';
+        return requestOptions;
+      } else {
+        _handleLogout();
+        throw DioException(
+          requestOptions: requestOptions,
+          response: Response(
+            requestOptions: requestOptions,
+            statusCode: 401,
+            statusMessage: 'Unauthorized',
+          ),
+        );
+      }
+    }
+    return requestOptions;
+  }
+
+  Future<bool> _fetchUpdateToken() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final refreshToken = prefs.getString('refreshToken');
+
+      final response = await _dio.post(
+        '$_baseUrl$_tokenRefreshUrl',
+        data: {'refresh_token': refreshToken},
+      );
+
+      if (response.statusCode == 200) {
+        final newToken = response.data['access_token'];
+        await prefs.setString('accessToken', newToken);
+        return true;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return false;
+  }
+
+  Future<String?> _fetchAccessToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('accessToken');
+  }
+
+  void _handleLogout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('accessToken');
+    await prefs.remove('refreshToken');
+
+    _routeService.router.go('/firstLaunch');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,102 @@
+import 'package:dio/dio.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'auth/token_authenticator.dart';
 import 'route/route_service.dart';
+
+void setupDio(Dio dio, TokenAuthenticator tokenAuthenticator) {
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest:
+          (RequestOptions options, RequestInterceptorHandler handler) async {
+        final token = await tokenAuthenticator.fetchAccessToken();
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+        handler.next(options);
+      },
+      onResponse:
+          (Response response, ResponseInterceptorHandler handler) async {
+        handler.next(response);
+      },
+      onError: (DioException e, ErrorInterceptorHandler handler) async {
+        if (e.response?.statusCode == 401) {
+          try {
+            final newOptions = await tokenAuthenticator.authenticate(
+              e.requestOptions,
+              e.response!,
+            );
+            final newResponse = await dio.request(
+              newOptions.path,
+              options: Options(
+                method: newOptions.method,
+                headers: newOptions.headers,
+              ),
+            );
+            handler.resolve(newResponse);
+          } catch (error) {
+            handler.reject(DioException(
+              requestOptions: e.requestOptions,
+              response: e.response,
+              error: error,
+            ));
+          }
+        } else {
+          handler.reject(e);
+        }
+      },
+    ),
+  );
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
+  final Dio dio = Dio();
+  final RouteService routeService = RouteService(
+    isFirstLaunch: false, // Placeholder, will be updated later
+  );
+  final TokenAuthenticator tokenAuthenticator = TokenAuthenticator(
+    dio,
+    'https://example.com/', // Base URL
+    'token/refresh', // Token refresh URL
+    routeService,
+  );
+
+  setupDio(dio, tokenAuthenticator);
+
   runApp(
     EasyLocalization(
-        supportedLocales: const [
-          Locale('en', 'US'),
-          Locale('ko', 'KR'),
-          Locale('ja', 'JP'),
-          Locale('zh', 'CN'),
-          Locale('vi', 'VN')
-        ],
-        path: 'assets/translations',
-        fallbackLocale: const Locale('en', 'US'),
-        child: const MainApp()),
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('ko', 'KR'),
+        Locale('ja', 'JP'),
+        Locale('zh', 'CN'),
+        Locale('vi', 'VN')
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      child: MainApp(
+        tokenAuthenticator: tokenAuthenticator,
+        routeService: routeService,
+      ),
+    ),
   );
 }
 
 class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+  final TokenAuthenticator tokenAuthenticator;
+  final RouteService routeService;
+
+  const MainApp({
+    super.key,
+    required this.tokenAuthenticator,
+    required this.routeService,
+  });
 
   @override
   State<MainApp> createState() => _MainAppState();
@@ -58,7 +129,11 @@ class _MainAppState extends State<MainApp> {
       isFirstLaunch: isFirstLaunch!,
     );
 
-    setState(() {});
+    setState(() {
+      _routeService = RouteService(
+        isFirstLaunch: isFirstLaunch!,
+      );
+    });
   }
 
   @override
