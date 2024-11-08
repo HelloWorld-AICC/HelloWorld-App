c15a0b99b4386a42789f09286cfe82a24faef620
wonpyoJang <jangwonpyo0@gmail.com>
2024-11-08 22:09:07 +0900

    merge: merged from main

--- lib/mypage/edit_profile/presentation/edit_profile_screen.dart
+++ lib/mypage/edit_profile/presentation/edit_profile_screen.dart
@@ -109,7 +109,46 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                             fontSize: 12,
                             fontWeight: FontWeight.w500,
                           ),
-                        )
+                        ),
+                        const SizedBox(height: 14),
+                        // GridView.count(
+                        //   primary: false,
+                        //   padding: EdgeInsets.zero,
+                        //   crossAxisCount: 4,
+                        //   children: <Widget>[
+                        //     Container(
+                        //       padding: const EdgeInsets.all(8),
+                        //       color: Colors.teal[100],
+                        //       child: const Text(
+                        //           "He'd have you all unravel at the"),
+                        //     ),
+                        //     Container(
+                        //       padding: const EdgeInsets.all(8),
+                        //       color: Colors.teal[200],
+                        //       child: const Text('Heed not the rabble'),
+                        //     ),
+                        //     Container(
+                        //       padding: const EdgeInsets.all(8),
+                        //       color: Colors.teal[300],
+                        //       child: const Text('Sound of screams but the'),
+                        //     ),
+                        //     Container(
+                        //       padding: const EdgeInsets.all(8),
+                        //       color: Colors.teal[400],
+                        //       child: const Text('Who scream'),
+                        //     ),
+                        //     Container(
+                        //       padding: const EdgeInsets.all(8),
+                        //       color: Colors.teal[500],
+                        //       child: const Text('Revolution is coming...'),
+                        //     ),
+                        //     Container(
+                        //       padding: const EdgeInsets.all(8),
+                        //       color: Colors.teal[600],
+                        //       child: const Text('Revolution, they...'),
+                        //     ),
+                        //   ],
+                        // ),
                       ],
                     ))
                   ],
@@ -120,5 +159,14 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         );
       }),
     );
+  }
+}
+
+class LanguageItem extends StatelessWidget {
+  const LanguageItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
